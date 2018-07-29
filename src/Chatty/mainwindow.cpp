fcb4337b73a4fbda63b5837a5eff9be0cd373257
Theodore <elvisxiaozhi@gmail.com>
2018-07-29 21:43:03 +0800

    add basic layout

---
+++ src/Chatty/mainwindow.cpp
@@ -0,0 +1,26 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QStatusBar>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    setWindowLayout();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::setWindowLayout()
+{
+    ui->msBox->setReadOnly(true);
+    ui->msBox->setStyleSheet("border: 1px solid black");
+
+    ui->userList->setStyleSheet("border: 1px solid black");
+
+    statusBar()->showMessage(tr("Offline"));
+}
